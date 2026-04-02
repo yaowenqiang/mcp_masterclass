a2fc61233a536eea05ef27b80c7d17885c7711a5
yaowenqiang <yaowenqiang111@163.com>
2026-04-02 23:24:25 +0800

    add more resources demo

--- resources.py
+++ resources.py
@@ -2,6 +2,17 @@ from fastmcp import FastMCP
 
 mcp = FastMCP('recources')
 
+inventory_id_to_price = {
+    "123": "6.99",
+    "456": "17.99",
+    "789": "84.99",
+}
+inventory_name_to_id = {
+    "Coffee":"123",
+    "Tea":"456",
+    "Cookies":"789",
+}
+
 @mcp.resource('inventory://overview')
 def get_inventory_overview() -> str:
     """
@@ -18,3 +29,17 @@ def get_inventory_overview() -> str:
     """
 
     return overview.strip()
+
+@mcp.resource('inventory://{inventory_id}/price')
+def get_inventory_price_from_inventory_id(inventory_id: str) -> str:
+    """
+    Returns price from inventory id
+    """
+    return inventory_id_to_price[inventory_id]
+
+@mcp.resource('inventory://{inventory_name}/id')
+def get_inventory_id_from_inventory_name(inventory_name: str) -> str:
+    """
+    Returns id from inventory name
+    """
+    return inventory_name_to_id[inventory_name]
