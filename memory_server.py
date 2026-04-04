dcf60517564ebacec5fb282e5782bffee04b8f53
yaowenqiang <yaowenqiang111@163.com>
2026-04-04 23:30:32 +0800

    add mcp server demo

---
+++ memory_server.py
@@ -0,0 +1,38 @@
+from fastmcp import FastMCP
+from openai import OpenAI
+import tempfile
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI()
+
+VECTOR_STORE_NAME = 'MEMORIES'
+mcp = FastMCP('Memories')
+
+def get_or_create_vector_store():
+    # Try to find existing vector store, else create
+    stores = client.vector_stores.list()
+
+    for store in stores:
+        if store.name == VECTOR_STORE_NAME:
+            return store
+    return client.vector_stores.create(name=VECTOR_STORE_NAME)
+
+
+@mcp.tool
+def save_memory(memory: str):
+    """Save a memory string to the vector store."""
+    vector_store = get_or_create_vector_store()
+
+    with tempfile.NamedTemporaryFile(mode='w+', delete=False,suffix='.txt') as f:
+        f.write(memory)
+        f.flush()
+
+        client.vector_stores.files.upload_and_poll(
+            vector_store_id=vector_store.id,
+            file=open(f.name, 'rb')
+        )
+
+    return {'staus':'saved','vector_store_id': vector_store.id}
