a1a8c505322bafb924a5ae436132d00ba1b00ab3
yaowenqiang <yaowenqiang111@163.com>
2026-04-05 10:41:32 +0800

    add more memory mcp tools

--- memory_server.py
+++ memory_server.py
@@ -36,3 +36,20 @@ def save_memory(memory: str):
         )
 
     return {'staus':'saved','vector_store_id': vector_store.id}
+
+
+@mcp.tool
+def search_memory(query: str):
+    """Search memories in the vector store and return relevant chunks"""
+    vector_store = get_or_create_vector_store()
+    results = client.vector_stores.search(
+    vector_store_id=vector_store.id,
+    queyr=query,
+    )
+
+    content_texts = [
+            content.text for item in results.data
+            for content in item.content
+            if content.type = 'text'
+    }
+    return {results: content_texts}
